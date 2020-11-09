f4e60ae303b969e5d56a43beea7451e5451c25e1
RoanKanninga <roankanninga@gmail.com>
2020-11-09 14:46:41 +0100

    fixing indentation issues

--- bin/ParseDarwinSamplesheet.sh
+++ bin/ParseDarwinSamplesheet.sh
@@ -78,16 +78,16 @@ EOH
 }
 
 array_contains () {
-    local array="$1[@]"
-    local seeking="${2}"
-    local in=1
-    for element in "${!array-}"; do
-        if [[ "${element}" == "${seeking}" ]]; then
-            in=0
-            break
-        fi
-    done
-    return "${in}"
+	local array="$1[@]"
+	local seeking="${2}"
+	local in=1
+	for element in "${!array-}"; do
+		if [[ "${element}" == "${seeking}" ]]; then
+			in=0
+			break
+		fi
+	done
+	return "${in}"
 }
 
 #

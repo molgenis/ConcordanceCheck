35f69e315a881f9892b6ac5a51f8a60517ee83ab
RoanKanninga <roankanninga@gmail.com>
2022-08-31 11:42:40 +0200

    make it wingedhelix proof

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -168,7 +168,7 @@ do
 	log4Bash 'DEBUG' "${LINENO}" "${FUNCNAME:-main}" '0' \
 	"check ${ConcordanceID}, ngsId:${ngsId} and arrayId:${arrayId} ngsDnaNo:${ngsDnaNo}, arrayDnaNo:${arrayDnaNo}"
 	
-	if ssh -n "${ATEAMBOTUSER}@${HOSTNAME_PRM}" test -e "/groups/${group}/${PRM_LFS}/concordance/logs/${ConcordanceID}.copyConcordanceCheckData.finished"
+	if ls "${concordanceDir}/logs/${ConcordanceID}"*".ConcordanceCheck.finished" 2>/dev/null
 	then
 		log4Bash 'DEBUG' "${LINENO}" "${FUNCNAME:-main}" '0' \
 			"rm -rf ${concordanceDir}/jobs/${ConcordanceID}.*/ 

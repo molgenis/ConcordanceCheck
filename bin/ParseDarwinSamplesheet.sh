2fe1d926af28f1fda181ad6a79705120b849005a
Gerbenvandervries <gerbenvandervries@gmail.com>
2024-07-19 12:27:14 +0000

    try

--- bin/ParseDarwinSamplesheet.sh
+++ bin/ParseDarwinSamplesheet.sh
@@ -128,7 +128,7 @@ fetch_data () {
 			_searchPath=("/groups/${NGSGROUP}/prm0"*"/projects/${_project}"*"/run01/results/alignment/")
 			
 			#fetch filename and path, and store in ${_sampleId} ${_filePath}, set _fileType to CRAM
-			_filePath="$(fetch "${_sample}" "\(.bam\|.bam.cram\)" "${_searchPath[0]}")" || exit
+			_filePath="$(fetch "${_sample}" "\(.bam\|.bam.cram\)" "${_searchPath[0]}")"
 			_sampleId="$(basename "${_filePath}" ".merged.dedup.bam.cram")"
 			_sampleId="$(basename "${_sampleId}" ".merged.dedup.bam")"
 			if [[ "${_filePath}" == *"cram"* ]]
@@ -148,7 +148,7 @@ fetch_data () {
 		if [[ -d "${_searchPath[0]}" ]]
 		then
 			#fetch filename and path, and store in ${_sampleId} ${_filePath}, set _fileType to VCF
-			_filePath="$(fetch "${_sample}" ".concordance.vcf.gz" "${_searchPath[0]}")" || exit
+			_filePath="$(fetch "${_sample}" ".concordance.vcf.gz" "${_searchPath[0]}")"
 			_sampleId="$(basename "${_filePath}" ".concordance.vcf.gz")"
 			_fileType='VCF'
 
@@ -158,7 +158,7 @@ fetch_data () {
 			_searchPath=("/groups/${NGSGROUP}/prm0"*"/projects/${_project}"*"/run01/results/alignment/")
 
 			#fetch filename and path, and store in ${_sampleId} ${_filePath}, set _fileType to CRAM
-			_filePath="$(fetch "${_sample}" ".sorted.merged.bam" "${_searchPath[0]}")" || exit
+			_filePath="$(fetch "${_sample}" ".sorted.merged.bam" "${_searchPath[0]}")"
 			_sampleId="$(basename "${_filePath}" ".sorted.merged.bam")"
 			_fileType='BAM'
 		else
@@ -170,7 +170,7 @@ fetch_data () {
 		_searchPath=("/groups/${NGSGROUP}/dat0"*"/openarray/"*"${_project}"*"/")
 
 		#fetch filename and path, and store in ${_sampleId} ${_filePath}, set _fileType to OA
-		_filePath="$(fetch "${_sample}" ".oarray.txt" "${_searchPath[0]}")" || exit
+		_filePath="$(fetch "${_sample}" ".oarray.txt" "${_searchPath[0]}")"
 		_sampleId="$(basename "${_filePath}" ".oarray.txt")"
 		_fileType='OPENARRAY'
 
@@ -179,7 +179,7 @@ fetch_data () {
 		_searchPath=("/groups/${NGSGROUP}/prm0"*"/projects/"*"${_project}"*"/run01/results/intermediates/")
 
 		#fetch filename and path, and store in ${_sampleId} ${_filePath}, set _fileType to OA
-		_filePath="$(fetch "${_sample}" ".cram" "${_searchPath[0]}")" || exit
+		_filePath="$(fetch "${_sample}" ".cram" "${_searchPath[0]}")"
 		_sampleId="$(basename "${_filePath}" ".cram")"
 		_fileType='CRAM'
 	else

80ba78ee6f4c272936a60ed155974e13816dad87
Gerbenvandervries <gerbenvandervries@gmail.com>
2025-04-18 14:25:00 +0000

    test

--- nextflow/test/autotest.sh
+++ nextflow/test/autotest.sh
@@ -14,25 +14,25 @@ TEMP="${WORKDIR}/temp"
 echo "cleaning up.."
 rm -rvf "${WORKDIR}/${pipeline}"
 
-	echo "new pull request for ConcordanceCheck"
-	rm -rf "${WORKDIR}"
-	mkdir -p "${WORKDIR}/jobs"
-	mkdir -p "${WORKDIR}/results"
-	mkdir -p "${WORKDIR}/ngs"
-	mkdir -p "${WORKDIR}/logs/${pipeline}"
-	mkdir -p "${WORKDIR}/tmp"
-	mkdir -p "${WORKDIR}/samplesheets/archive"
+echo "new pull request for ConcordanceCheck"
+rm -rf "${WORKDIR}"
+mkdir -p "${WORKDIR}/jobs"
+mkdir -p "${WORKDIR}/results"
+mkdir -p "${WORKDIR}/ngs"
+mkdir -p "${WORKDIR}/logs/${pipeline}"
+mkdir -p "${WORKDIR}/tmp"
+mkdir -p "${WORKDIR}/samplesheets/archive"
 
-	cd "${WORKDIR}"
-	git clone "https://github.com/molgenis/${pipeline}.git"
+cd "${WORKDIR}"
+git clone "https://github.com/molgenis/${pipeline}.git"
 
-	cd "${pipeline}" || exit
-	git fetch --tags --progress "https://github.com/molgenis/${pipeline}/" +refs/pull/*:refs/remotes/origin/pr/*
-	COMMIT=$(git rev-parse refs/remotes/origin/pr/${PULLREQUEST}/merge^{commit})
-	echo "checkout commit: COMMIT"
-	git checkout -f "${COMMIT}"
+cd "${pipeline}" || exit
+git fetch --tags --progress "https://github.com/molgenis/${pipeline}/" +refs/pull/*:refs/remotes/origin/pr/*
+COMMIT=$(git rev-parse refs/remotes/origin/pr/${PULLREQUEST}/merge^{commit})
+echo "checkout commit: COMMIT"
+git checkout -f "${COMMIT}"
 
-	mv nextflow ../
+mv nextflow ../
 
 ## copy samplesheets to ${TMPDIRECTORY}/samplesheets/
 cp -v "${WORKDIR}/nextflow/test/samplesheets/"*".sampleId.txt" "${WORKDIR}/samplesheets/"
@@ -45,4 +45,3 @@ module load ${pipeline}/betaAutotest
 "${WORKDIR}"/ConcordanceCheck/bin/ConcordanceCheck.sh -g umcg-atd -w "${WORKDIR}"
 
 echo -e "\n Test succeeded!!\n"
-fi

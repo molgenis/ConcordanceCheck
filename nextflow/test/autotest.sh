a1b6deaab7aa3c17ad93413d8cd9a3659a32725f
Gerbenvandervries <gerbenvandervries@gmail.com>
2025-05-15 07:22:59 +0000

    user fix

--- nextflow/test/autotest.sh
+++ nextflow/test/autotest.sh
@@ -103,7 +103,7 @@ echo "Now starting the pipeline"
 module load nextflow
 module load ${pipeline}/betaAutotest
 perl -pi -e 's|sleep 15|sleep 1|g' "${WORKDIR}"/ConcordanceCheck/bin/ConcordanceCheck.sh
-perl -pi -e 's|ATEAMBOTUSER|umcg-molgenis|g' "${WORKDIR}"/ConcordanceCheck/etc/sharedConfig.cfg
+perl -pi -e 's|\${GROUP}-ateambot|umcg-molgenis|g' "${WORKDIR}"/ConcordanceCheck/etc/sharedConfig.cfg
 
 "${WORKDIR}"/ConcordanceCheck/bin/ConcordanceCheck.sh -g umcg-atd -w "${WORKDIR}" 2>&1 | tee -a "${WORKDIR}/tmp/ConcordanceCheck.log"
 
